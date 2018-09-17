50634783091fd0a5268079babbc8e0f234e562a7
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-09-17 13:27:32 +1000

    build: rename app -> resources

--- vars/build.groovy
+++ vars/build.groovy
@@ -3,24 +3,36 @@ import io.openshift.Utils
 
 def call(Map args) {
     stage("Build application") {
-        def namespace = args.namespace ?: Utils.usersNamespace()
-        def image = config.runtime() ?: 'oc'
+      def namespace = args.namespace ?: Utils.usersNamespace()
+      def image = config.runtime() ?: 'oc'
+      def res = args.resources
+      if (!res) {
+        error "Missing manadatory parameter: resources"
+        currentBuild.result = 'ABORTED'
+      }
 
-        def status = ""
-        spawn(image: image, version: config.version(), commands: args.commands) {
-            Events.emit("build.start")
-            try {
-              createImageStream(args.app.ImageStream, namespace)
-              buildProject(args.app.BuildConfig, namespace)
-              status = "pass"
-            } catch (e) {
-              status = "fail"
-              echo "build failed"
-              throw e
-            } finally {
-              Events.emit(["build.end", "build.${status}"], [status: status, namespace: namespace])
-            }
+      if (!res.ImageStream || !res.BuildConfig) {
+        def found = res.keySet().join(', ')
+        error "parameter resources must contain ImageStream and BuildConfig but found: $found"
+        currentBuild.result = 'ABORTED'
+        return
+      }
+
+      def status = ""
+      spawn(image: image, version: config.version(), commands: args.commands) {
+        Events.emit("build.start")
+        try {
+          createImageStream(res.ImageStream, namespace)
+          buildProject(res.BuildConfig, namespace)
+          status = "pass"
+        } catch (e) {
+          status = "fail"
+          echo "build failed"
+          throw e
+        } finally {
+          Events.emit(["build.end", "build.${status}"], [status: status, namespace: namespace])
         }
+      }
     }
 }
 
