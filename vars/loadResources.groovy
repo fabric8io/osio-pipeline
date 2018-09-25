df3690f409eb409d49c038f5501daf1a252b1531
Hrishikesh Shinde <hshinde@redhat.com>
2018-09-25 17:51:52 +0530

    Adds `loadResources` API
    This patch introduces a new API to read the resources from
    yaml file. This is useul to read configmap, secrets resources apart from
    OpenShft template.
    Updated deploy API to apply the configmap.
    
    Fixes
      - https://github.com/openshiftio/openshift.io/issues/4331

--- vars/loadResources.groovy
+++ vars/loadResources.groovy
@@ -5,12 +5,10 @@ def call(Map args = [:]) {
 
     def resources = readYaml(file: args.file)
 
-    if (resources instanceof LinkedList) {
-      return resources.groupBy({ r -> r.kind })
-    } else if (resources.kind == "List") {
+    if (resources instanceof Map && resources.kind == "List") {
       return resources.items.groupBy({ r -> r.kind })
     } else {
-      return [resources].groupBy({ r -> r.kind })
+      return [resources].flatten().groupBy({ r -> r.kind })
     }
 }
 
