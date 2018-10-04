c455fdb9aa08911aedd5c97d6e5afac04a5bda10
Yusuf Zainee <yzainee@redhat.com>
2018-10-04 16:19:32 +0530

    Adding node support for analytics plugin #4344
    The analytics needs the git url and the ecosystem to perform
    certain additional tasks to find the dependencies and then notify
    the user for any kind of vulnerabilities
    
    The analytics.groovy provides a handle to invoke the analytic's
    bayesian plugin wherein it passes the git hub url and the ecosystem
    
    Resolves: openshiftio/openshift.io#4344

--- src/io/openshift/plugins/analytics.groovy
+++ src/io/openshift/plugins/analytics.groovy
@@ -6,6 +6,24 @@ import io.openshift.Utils
 def register() {
     Events.on("build.pass") {
         e, a -> echo "invoking bayesian analytics $e $a"
+        def image = config.runtime() ?: ''
+        if(image != "") {
+            def url = Utils.shWithOutput(this, "git config remote.origin.url")
+            echo "URL:$url"
+            echo "Image:$image"
+            try {
+                retry(3) {
+                    def response = bayesianAnalysis url: 'https://bayesian-link', gitUrl: url, ecosystem: image
+                    if (response.success) {
+                      Utils.addAnnotationToBuild(this, 'fabric8.io/bayesian.analysisUrl', response.getAnalysisUrl())
+                    } else {
+                      echo "Bayesian analysis failed ${response}"
+                    }
+                }
+            } catch (err) {
+                echo "Unable to run Bayesian analysis: ${err}"
+            }
+        }
     }
 }
 
