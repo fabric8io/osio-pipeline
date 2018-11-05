51b14d20b0dd22db6fff76f2b690b47e5f1da875
Yusuf Zainee <yzainee@redhat.com>
2018-11-05 20:39:53 +0530

    temp commit. do not merge

--- src/io/openshift/plugins/analytics.groovy
+++ src/io/openshift/plugins/analytics.groovy
@@ -1,10 +1,27 @@
 package io.openshift.plugins
 
 import io.openshift.Events
+import io.openshift.Utils
 
 def register() {
     Events.on("build.pass") {
-        e, a -> echo "invoking bayesian analytics $e $a"
+        e, build -> echo "invoking bayesian analytics"
+        def image = config.runtime()
+        if(!image) {
+            return
+        }
+        try {
+            retry(3) {
+                def response = bayesianAnalysis(url: 'https://bayesian-link', gitUrl: build.git.url, ecosystem: image)
+                if (response.success) {
+                  Utils.addAnnotationToBuild(this, 'fabric8.io/bayesian.analysisUrl', response.analysisUrl)
+                } else {
+                  echo "Bayesian analysis failed ${response}"
+                }
+            }
+        } catch (err) {
+            echo "Unable to run Bayesian analysis: ${err}"
+        }
     }
 }
 
