21b1adf5c9d424e3cc2f5ac66c4b826e0093bf3c
Pradeep Kumar <pradeepitm12@gmail.com>
2018-11-21 18:31:57 +0530

    validates the resource (#65)
    Jenkins throws null pointer exception if any field is mising in resource file.
    
    This patch validates the resource file before processing it in `loadResources`
    API.
    The patch:
    - Gives an option to validate template by default validates.
    - Updates readme for `loadResource` API arguments.
    
    Resolves
     - https://github.com/fabric8io/osio-pipeline/issues/52

--- vars/loadResources.groovy
+++ vars/loadResources.groovy
@@ -1,9 +1,14 @@
+import io.openshift.Utils
+
 def call(Map args = [:]) {
-  if(!args.file) {
+  if (!args.file) {
     error "Missing manadatory parameter: file"
   }
 
-  def yaml = readYaml(file: args.file)
+  def validate = args.validate == false ? false : true
+  if (validate) {
+    Utils.shWithOutput(this, "oc apply --dry-run=true --validate=true -f $args.file");
+  }
 
   // resources can be:
   //  - a single resource
@@ -12,7 +17,7 @@ def call(Map args = [:]) {
   //    and items property is a list of resources
   //
   // for each of the above, loadResources must return [kind: [Resources, ...]]
-
+  def yaml = readYaml(file: args.file)
   def isListKind = yaml instanceof Map && yaml.kind == "List"
   def resources = isListKind ? yaml.items : [yaml].flatten()
   return resources.groupBy({ r -> r.kind })
