fabric8io/osio-pipeline

commit 58fca8465ff5c53dac978a4cc4899fcef8314276

first spike for unit test (#55)
- Adds `pom.xml` with build and test configuration
- Adds basic unit test for `Utils.mergeResources`

src/io/openshift/Utils.groovy
@@ -29,16 +29,16 @@ class Utils {
     }
   }
 
-  static String usersNamespace() {
-    def ns = currentNamespace()
+  static String usersNamespace(oc) {
+    def ns = currentNamespace(oc)
     if (ns.endsWith("-jenkins")) {
       return ns.substring(0, ns.lastIndexOf("-jenkins"))
     }
     return ns
   }
 
-  static String currentNamespace() {
-    OpenShiftClient oc = new DefaultOpenShiftClient()
+  static String currentNamespace(oc) {
+    oc = oc ?: new DefaultOpenShiftClient()
     return oc.getNamespace()
   }
 

.gitignore
@@ -1,0 +1,24 @@
+# ---> Java
+*.class
+
+# Mobile Tools for Java (J2ME)
+.mtj.tmp/
+
+# Package Files #
+*.jar
+*.war
+*.ear
+
+# virtual machine crash logs, see http://www.java.com/en/download/help/error_hotspot.xml
+hs_err_pid*
+
+.classpath
+.project
+.settings
+target
+build
+
+.gradle
+
+.idea/
+*.iml

assembly/assembly.xml
@@ -1,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<assembly>
+	<id>artifact</id>
+	<formats>
+		<format>zip</format>
+	</formats>
+	<baseDirectory>/</baseDirectory>
+	<fileSets>
+		<fileSet>
+			<directory>src</directory>
+			<outputDirectory>lib/src</outputDirectory>
+		</fileSet>
+		<fileSet>
+			<directory>vars</directory>
+			<outputDirectory>lib/vars</outputDirectory>
+		</fileSet>
+		<fileSet>
+			<directory>resources</directory>
+			<outputDirectory>lib/resources</outputDirectory>
+		</fileSet>
+
+	</fileSets>
+</assembly>

pom.xml
@@ -1,0 +1,250 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+
+  <groupId>io.openshift</groupId>
+  <artifactId>osio-pipeline</artifactId>
+  <version>0.1-SNAPSHOT</version>
+  <packaging>pom</packaging>
+
+  <organization>
+    <name>OpenShift.io</name>
+    <url>https://openshift.io</url>
+  </organization>
+
+  <prerequisites>
+    <maven>3.2.1</maven>
+  </prerequisites>
+
+  <repositories>
+    <repository>
+      <id>jenkins</id>
+      <url>http://repo.jenkins-ci.org/releases/</url>
+    </repository>
+  </repositories>
+
+  <properties>
+    <maven.compiler.source>1.8</maven.compiler.source>
+    <maven.compiler.target>1.8</maven.compiler.target>
+    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+    <workflow-job.verion>2.10</workflow-job.verion>
+    <groovy.version>2.4.12</groovy.version>
+    <groovy-json.version>2.4.3</groovy-json.version>
+    <kubernetes-client.version>4.0.4</kubernetes-client.version>
+    <junit.version>4.12</junit.version>
+    <jenkins-pipeline-unit.version>1.1</jenkins-pipeline-unit.version>
+    <jenkins-core.version>2.62</jenkins-core.version>
+    <groovy-cps.version>1.11</groovy-cps.version>
+    <javax.servlet-api.version>3.0.1</javax.servlet-api.version>
+    <mockwebserver.version>0.1.0</mockwebserver.version>
+  </properties>
+
+  <dependencyManagement>
+    <dependencies>
+      <dependency>
+        <groupId>org.jenkins-ci.plugins.workflow</groupId>
+        <artifactId>workflow-job</artifactId>
+        <version>${workflow-job.verion}</version>
+      </dependency>
+    </dependencies>
+  </dependencyManagement>
+
+  <dependencies>
+    <dependency>
+      <groupId>org.codehaus.groovy</groupId>
+      <artifactId>groovy-all</artifactId>
+      <version>${groovy.version}</version>
+    </dependency>
+
+    <dependency>
+      <groupId>org.codehaus.groovy</groupId>
+      <artifactId>groovy-json</artifactId>
+      <version>${groovy-json.version}</version>
+    </dependency>
+
+    <dependency>
+      <groupId>io.fabric8</groupId>
+      <artifactId>kubernetes-client</artifactId>
+      <version>${kubernetes-client.version}</version>
+    </dependency>
+
+    <dependency>
+      <groupId>io.fabric8</groupId>
+      <artifactId>openshift-client</artifactId>
+      <version>${kubernetes-client.version}</version>
+    </dependency>
+
+    <dependency>
+      <groupId>io.fabric8</groupId>
+      <artifactId>kubernetes-server-mock</artifactId>
+      <version>${kubernetes-client.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+    <dependency>
+      <groupId>io.fabric8</groupId>
+      <artifactId>openshift-server-mock</artifactId>
+      <version>${kubernetes-client.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+
+    <dependency>
+      <groupId>io.fabric8</groupId>
+      <artifactId>mockwebserver</artifactId>
+      <version>${mockwebserver.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+    <dependency>
+      <groupId>org.jenkins-ci.plugins.workflow</groupId>
+      <artifactId>workflow-job</artifactId>
+    </dependency>
+
+    <dependency>
+      <groupId>org.jenkins-ci.main</groupId>
+      <artifactId>jenkins-core</artifactId>
+      <version>${jenkins-core.version}</version>
+      <scope>provided</scope>
+    </dependency>
+
+    <dependency>
+      <groupId>com.cloudbees</groupId>
+      <artifactId>groovy-cps</artifactId>
+      <version>${groovy-cps.version}</version>
+    </dependency>
+
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>${junit.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+    <dependency>
+      <groupId>com.lesfurets</groupId>
+      <artifactId>jenkins-pipeline-unit</artifactId>
+      <version>${jenkins-pipeline-unit.version}</version>
+      <scope>test</scope>
+    </dependency>
+
+    <dependency>
+      <groupId>javax.servlet</groupId>
+      <artifactId>javax.servlet-api</artifactId>
+      <version>${javax.servlet-api.version}</version>
+      <scope>provided</scope>
+    </dependency>
+  </dependencies>
+
+  <build>
+    <sourceDirectory>${project.basedir}/src</sourceDirectory>
+    <plugins>
+      <plugin>
+        <artifactId>maven-resources-plugin</artifactId>
+        <version>3.0.2</version>
+        <executions>
+          <execution>
+            <id>copy-test-resources</id>
+            <phase>validate</phase>
+            <goals>
+              <goal>testResources</goal>
+            </goals>
+            <configuration>
+              <outputDirectory>${basedir}/target/test-classes/osio-pipeline@master</outputDirectory>
+              <resources>
+                <resource>
+                  <directory>${project.basedir}</directory>
+                  <filtering>true</filtering>
+                  <includes>
+                    <include>src/**/*.*</include>
+                    <include>vars/**/*.*</include>
+                    <include>resources/**/*.*</include>
+                  </includes>
+                </resource>
+              </resources>
+            </configuration>
+          </execution>
+        </executions>
+      </plugin>
+
+      <!-- Tests -->
+      <plugin>
+        <groupId>org.codehaus.gmavenplus</groupId>
+        <artifactId>gmavenplus-plugin</artifactId>
+        <version>1.6</version>
+        <executions>
+          <execution>
+            <goals>
+              <goal>addSources</goal>
+              <goal>addTestSources</goal>
+              <goal>compile</goal>
+              <goal>compileTests</goal>
+            </goals>
+          </execution>
+        </executions>
+        <configuration>
+          <sources>
+            <source>
+              <directory>${project.basedir}/src</directory>
+              <includes>
+                <include>**/*.groovy</include>
+              </includes>
+            </source>
+          </sources>
+          <testSources>
+            <source>
+              <directory>${project.basedir}/test/groovy</directory>
+              <includes>
+                <include>**/*.groovy</include>
+              </includes>
+            </source>
+          </testSources>
+        </configuration>
+      </plugin>
+      <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-surefire-plugin</artifactId>
+        <version>2.20</version>
+        <configuration>
+          <failIfNoTests>true</failIfNoTests>
+        </configuration>
+        <executions>
+          <execution>
+            <id>test</id>
+            <phase>test</phase>
+            <configuration>
+              <includes>
+                <include>**/*Test.java</include>
+                <include>**/*Test.groovy</include>
+              </includes>
+            </configuration>
+            <goals>
+              <goal>test</goal>
+            </goals>
+          </execution>
+        </executions>
+      </plugin>
+
+      <!-- Package -->
+      <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-assembly-plugin</artifactId>
+        <version>3.0.0</version>
+        <executions>
+          <execution>
+            <id>create-distribution</id>
+            <phase>package</phase>
+            <goals>
+              <goal>single</goal>
+            </goals>
+            <configuration>
+              <descriptors>
+                <descriptor>assembly/assembly.xml</descriptor>
+              </descriptors>
+            </configuration>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+</project>

test/groovy/PipelineHelper.groovy
@@ -1,0 +1,35 @@
+import com.lesfurets.jenkins.unit.BasePipelineTest
+import org.junit.Before
+
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+import static com.lesfurets.jenkins.unit.global.lib.LocalSource.localSource
+
+
+class PipelineHelper extends BasePipelineTest {
+
+  @Override
+  @Before
+  void setUp() throws Exception {
+    plugSharedPipelineLib()
+
+    // its intended to call it later
+    super.setUp()
+  }
+
+  def plugSharedPipelineLib() {
+    String sharedLibs = this.class.getResource('./').getFile()
+
+    def library = library()
+      .name('osio-pipeline')
+      .allowOverride(false)
+      .retriever(localSource(sharedLibs))
+      .targetPath(sharedLibs)
+      .defaultVersion("master")
+      .implicit(true)
+      .build()
+    helper.registerSharedLibrary(library)
+
+    setScriptRoots(['src', 'vars', 'test/groovy'] as String[])
+    setScriptExtension('groovy')
+  }
+}

test/groovy/utils/UtilTest.groovy
@@ -1,0 +1,58 @@
+#!/usr/bin/env groovy
+import io.fabric8.kubernetes.client.ConfigBuilder
+import io.fabric8.openshift.client.DefaultOpenShiftClient
+import io.fabric8.openshift.client.OpenShiftClient
+import org.junit.Assert
+import org.junit.Test
+
+class UtilTest extends PipelineHelper {
+
+  @Test
+  void should_merge_maps_with_different_keys() throws Exception {
+    //GIVEN
+    Script script = loadScript("utils/jenkinsfiles/mergemaps.jenkinsfile")
+
+    // WHEN
+    runScript(script)
+
+    // THEN
+    assertStepExecutes("echo", "Different Keys : [a:[b], b:[d]])")
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void should_merge_maps_with_same_keys() throws Exception {
+    //GIVEN
+    Script script = loadScript("utils/jenkinsfiles/mergemaps.jenkinsfile")
+
+    // WHEN
+    runScript(script)
+
+    // THEN
+    assertStepExecutes("echo", "Same Keys : [a:[b, d]])")
+    assertJobStatusSuccess()
+  }
+
+  @Test
+  void get_username_space() throws Exception {
+    //GIVEN
+    Script script = loadScript("utils/jenkinsfiles/usernamespace.jenkinsfile")
+    OpenShiftClient oc = new DefaultOpenShiftClient(new ConfigBuilder().withNamespace("user-jenkins").build())
+    binding.setVariable("oc", oc)
+
+    // WHEN
+    runScript(script)
+
+    // THEN
+    assertStepExecutes("echo", "Namespace : user")
+    assertJobStatusSuccess()
+  }
+
+  def assertStepExecutes(String method, String value) {
+    Assert.assertTrue(helper.callStack.findAll { call ->
+      call.methodName == method
+    }.any { call ->
+      call.toString().contains(value)
+    })
+  }
+}

test/groovy/utils/jenkinsfiles/buildannotation.jenkinsfile
@@ -1,0 +1,15 @@
+#!/usr/bin/env groovy
+
+import com.lesfurets.jenkins.unit.global.lib.Library
+
+@Library('osio-pipeline@master') _
+
+import io.openshift.Utils
+
+node() {
+    Utils utils = new Utils()
+
+    stage("Add annotation to current build") {
+      utils.addAnnotationToBuild(this, "testannotation", "test")
+    }
+}

test/groovy/utils/jenkinsfiles/mergemaps.jenkinsfile
@@ -1,0 +1,19 @@
+#!/usr/bin/env groovy
+
+import com.lesfurets.jenkins.unit.global.lib.Library
+
+@Library('osio-pipeline@master') _
+
+import io.openshift.Utils
+
+node() {
+    Utils utils = new Utils()
+
+    stage("Merge Resources With different Keys") {
+      echo "Different Keys : ${utils.mergeResources([["a":"b"], ["b": "d"]])}"
+    }
+
+    stage("Merge Resources With Same Keys") {
+      echo "Same Keys : ${utils.mergeResources([["a":"b"], ["a": "d"]])}"
+    }
+}

commit 3ff95faf22d531095731d8302fa8b36dbac073fe

fixes usersNamespace in Utils.groovy
Changes in test case commit introduced
a bug in `usersNamespace` API which is
failing `build` API.

This patch fixes the regression happen
in https://github.com/fabric8io/osio-pipeline/pull/55

src/io/openshift/Utils.groovy
@@ -29,7 +29,7 @@ class Utils {
     }
   }
 
-  static String usersNamespace(oc) {
+  static String usersNamespace(oc = null) {
     def ns = currentNamespace(oc)
     if (ns.endsWith("-jenkins")) {
       return ns.substring(0, ns.lastIndexOf("-jenkins"))
@@ -37,7 +37,7 @@ class Utils {
     return ns
   }
 
-  static String currentNamespace(oc) {
+  static String currentNamespace(oc = null) {
     oc = oc ?: new DefaultOpenShiftClient()
     return oc.getNamespace()
   }