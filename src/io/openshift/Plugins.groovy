bcd2a2cdb002ba6b41479a4449a7aac8a5f1c804
Piyush Garg <piyushgarg001@gmail.com>
2019-01-24 18:50:28 +0530

    Feature to register analytics plugin only if available
    This patch will add a feature to register
    Analytics Plugin only if it is there in jenkins
    
    Fixes https://github.com/fabric8io/osio-pipeline/issues/74

--- src/io/openshift/Plugins.groovy
+++ src/io/openshift/Plugins.groovy
@@ -2,12 +2,13 @@ package io.openshift
 
 import io.openshift.plugins.analytics
 import io.openshift.Globals
+import static io.openshift.Utils.pluginAvailable
 
 class Plugins implements Serializable {
     static def register() {
       // analytics plugins is enabled by default
       Globals.plugins["analytics"] = Globals.plugins["analytics"] ?: [disabled: false]
-      if (Globals.plugins["analytics"].disabled ?: false) {
+      if (Globals.plugins["analytics"].disabled ?: false || !pluginAvailable("bayesian")) {
         return
       }
       new analytics().register()
