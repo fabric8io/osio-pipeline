5a3dc1480ca3c2384c939ef2be6508d110c4b5ca
Piyush Garg <19270240+piyush-garg@users.noreply.github.com>
2019-02-13 19:13:16 +0530

    Add plugins API to README.MD (#132)
    * Add plugins API to README.MD
    
    This adds the plugins API to README.MD and
    includes minor changes to the doc

--- src/io/openshift/Plugins.groovy
+++ src/io/openshift/Plugins.groovy
@@ -5,6 +5,7 @@ import io.openshift.Globals
 
 class Plugins implements Serializable {
     static def register() {
+      // analytics plugins is enabled by default
       Globals.plugins["analytics"] = Globals.plugins["analytics"] ?: [disabled: false]
       if (Globals.plugins["analytics"].disabled ?: false) {
         return
