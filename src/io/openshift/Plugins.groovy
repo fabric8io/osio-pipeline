405a3dbe07c00d98e0f5a2d9500474b66ef9489b
Piyush Garg <19270240+piyush-garg@users.noreply.github.com>
2019-02-13 11:56:23 +0530

    Add plugins api to provide plugins configuration (#126)
    This adds a `plugins` which can be used to
    provide configuration related to plugins
    
    Like you can provide
    
    plugins analytics: ["disabled" : true] to disable analytics
    
    Fixes https://github.com/fabric8io/osio-pipeline/issues/74

--- src/io/openshift/Plugins.groovy
+++ src/io/openshift/Plugins.groovy
@@ -1,8 +1,14 @@
 package io.openshift
-import io.openshift.plugins.*
+
+import io.openshift.plugins.analytics
+import io.openshift.Globals
 
 class Plugins implements Serializable {
     static def register() {
+      Globals.plugins["analytics"] = Globals.plugins["analytics"] ?: [disabled: false]
+      if (Globals.plugins["analytics"].disabled ?: false) {
+        return
+      }
       new analytics().register()
     }
 }
